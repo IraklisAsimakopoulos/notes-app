22bdc0abb232caab4492c68be40bc5c0a1b941b1
Iraklis Asimakopoulos <i.asimakopoulos@dataverse.gr>
2025-04-01 02:05:58 +0300

    feat: added link for googleapi icons and made an Icon component

---
+++ components/Icon.vue
@@ -0,0 +1,20 @@
+<script setup>
+import { useAttrs } from "vue";
+
+const props = defineProps({
+  iconName: {
+    type: String,
+    default: "",
+  },
+  size: {
+    type: String,
+    default: "6",
+  },
+});
+</script>
+
+<template>
+  <i :class="['material-icons', `w-${size} h-${size}`]">
+    {{ iconName }}
+  </i>
+</template>
