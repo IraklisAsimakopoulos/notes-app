88d0637fb81abe913a579626d777c643b8a34428
Iraklis Asimakopoulos <i.asimakopoulos@dataverse.gr>
2025-04-03 00:36:30 +0300

    feat: component Header

---
+++ components/Header.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="px-[24px] py-[10px] h-[79px] flex items-center">
+    <div
+      class="border-[2px] border-[#0A0C11] text-[#0A0C11] text-[22px] leading-[32px] font-[900] px-[13px] py-[3px] w-[173px] rounded-[8px]"
+    >
+      {{ title }}
+    </div>
+  </div>
+</template>
+
+<script setup>
+const props = defineProps({
+  title: {
+    type: String,
+    default: "Test project",
+  },
+});
+</script>
