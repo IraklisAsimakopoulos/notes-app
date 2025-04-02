fe0b40688ad2c0b2509a624a39f409092813ecd7
Iraklis Asimakopoulos <i.asimakopoulos@dataverse.gr>
2025-04-03 00:54:36 +0300

    feat: new ActionBar component

---
+++ components/ActionBar.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="flex justify-between">
+    <span class="text-[#141414] text-[22px] font-bold leading-[32px]">{{
+      title
+    }}</span>
+    <div class="flex gap-[16px]">
+      <FilterButton :onChange="onChange" />
+      <Button @click="handleToggleForm" icon="add">Add New</Button>
+    </div>
+  </div>
+  <NoteForm :open="isOpen" :onClose="handleCloseForm" />
+</template>
+
+<script setup>
+import { ref } from "vue";
+
+const props = defineProps({
+  title: {
+    type: String,
+    default: "Title",
+  },
+  onChange: {
+    type: Function,
+    default: null,
+  },
+});
+
+const isOpen = ref(false);
+
+const handleCloseForm = () => {
+  isOpen.value = false;
+};
+
+const handleToggleForm = () => {
+  isOpen.value = !isOpen.value;
+};
+</script>
